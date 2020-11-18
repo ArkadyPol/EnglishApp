0ef737b237dcb6d377f247fce8542f5ce55b423b
Arkady Polhovsky <arkims@inbox.ru>
2020-11-18 13:23:30 +0300

    init new project

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+const common = {
+  context: path.resolve(__dirname, 'src'),
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
+
+const frontend = {
+  entry: ['./index.tsx'],
+  output: {
+    filename: 'main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
+
+const backend = {
+  entry: ['./server.ts'],
+  output: {
+    filename: 'server.js',
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  plugins: [new NodemonPlugin()],
+};
+
+module.exports = [
+  { ...common, ...frontend },
+  { ...common, ...backend },
+];
