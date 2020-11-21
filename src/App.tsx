4068e03891f74992c381cd6259b2d3989c7176b5
Arkady Polhovsky <arkims@inbox.ru>
2020-11-21 20:07:09 +0300

    add header and content components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import styles from'./App.module.scss';
+import styles from './App.module.scss';
+import Content from './components/Content/Content';
+import Header from './components/Header/Header';
 
 const App = () => {
-  return <div className={styles.app}>English App</div>;
+  return (
+    <div className={styles.app}>
+      <Header />
+      <Content />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return <nav className={styles.header}></nav>;
+};
+
+export default Header;
