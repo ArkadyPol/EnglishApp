1b8c7126edb9172ec0f73805f49127e232bb1686
Arkady Polhovsky <arkims@inbox.ru>
2020-11-27 12:08:20 +0300

    add env loader

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '.')));
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
