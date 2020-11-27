6d7f4c78a1f6642b85d3b1dcb1fdb67a53710290
Arkady Polhovsky <arkims@inbox.ru>
2020-11-27 12:41:07 +0300

    install mysql2

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,14 @@
 import 'dotenv/config';
+import mysql from 'mysql2';
 import express from 'express';
 import path from 'path';
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASS,
+});
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '.')));
@@ -9,3 +17,4 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+connection.end();
