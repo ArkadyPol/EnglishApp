33018b2767db9e58c360972d579a0259da9952ae
Arkady Polhovsky <arkims@inbox.ru>
2020-11-28 11:34:08 +0300

    change mysql config

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,12 +3,15 @@ import mysql from 'mysql2';
 import express from 'express';
 import path from 'path';
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASS,
-});
+const pool = mysql
+  .createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+  })
+  .promise();
 
 const app = express();
 app.use(express.static(path.resolve(__dirname, '.')));
@@ -17,4 +20,3 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-connection.end();
