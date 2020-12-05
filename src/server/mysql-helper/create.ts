be92cff37e6db25ebad52578ed890e4088218a9e
Arkady Polhovsky <arkims@inbox.ru>
2020-12-05 06:42:34 +0300

    create temp table sorted sentences

--- src/server/mysql-helper/create.ts
+++ src/server/mysql-helper/create.ts
@@ -1,15 +1,32 @@
 import { Pool } from 'mysql2/promise';
 
-export const createUniqueWords = async (pool: Pool) => {
+const createUniqueWords = async (pool: Pool) => {
   const sql = `CREATE TEMPORARY TABLE IF NOT EXISTS UniqueWords AS
     SELECT (@row_number:=@row_number + 1) AS Num, Word, WordCount 
         FROM (SELECT Word, Count(WordID) AS WordCount FROM Words 
     GROUP BY Word
-    ORDER BY Count(WordID) DESC) t1,
+    ORDER BY Count(WordID) DESC, Word) t1,
     (SELECT @row_number:=0) t2;`;
+  const [rows] = await pool.query(sql);
+  console.log(rows);
+};
+
+const createSortedSentences = async (pool: Pool) => {
+  const sql = `CREATE TEMPORARY TABLE IF NOT EXISTS SortedSentences AS
+  SELECT Words.SentenceID, Sentences.Sentence, MAX(UniqueWords.Num) as Num
+  From Words
+  INNER JOIN UniqueWords ON Words.Word=UniqueWords.Word
+  INNER JOIN Sentences ON Words.SentenceID=Sentences.SentenceID
+  GROUP BY Words.SentenceID
+  ORDER BY Num`;
+  const [rows] = await pool.query(sql);
+  console.log(rows);
+};
+
+export const createTempTables = async (pool: Pool) => {
   try {
-    const [rows] = await pool.query(sql);
-    console.log(rows);
+    await createUniqueWords(pool);
+    await createSortedSentences(pool);
   } catch (err) {
     console.error(err);
   }
